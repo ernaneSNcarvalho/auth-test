77a40203423fd102da272d80be427853c0d134e0
Ernane <ernanecarvalho89@gmail.com>
2022-07-26 06:45:53 -0300

    JWT

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,25 +1,24 @@
 import {Request, Response, NextFunction} from 'express';
 import { User } from '../models/User';
+import JWT from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const Auth = {
     private: async (req: Request, res: Response, next: NextFunction) => {
         let success = false;
         // Fazer verificacao do auth        
         if(req.headers.authorization){
-            let hash = req.headers.authorization.substring(6);
-            let decoded: string = Buffer.from(hash, 'base64').toString();
-            console.log('decoded', decoded);
-            let data: string[] = decoded.split(':');
-            if(data.length === 2){
-                let hasUser = await User.findOne({
-                    where: {
-                        email: data[0],
-                        password: data[1]
-                    }
-                });
-                if(hasUser){
+            const [authType, token] = req.headers.authorization.split(' ');
+            if(authType === 'Bearer'){
+                try{
+                    const decoded = JWT.verify(token, process.env.JWT_SECRET_KEY as string);
                     success = true;
-                }
+                }catch(err){
+                    console.log("Err", "Erro no JWT");
+                }               
+                
             }
         }
         if(success){
